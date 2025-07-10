ab0ad194a714ed577645bffd5ece3fba0219cd8e
brittany-potato <brittanycc@missionreadyhq.com>
2025-07-10 23:36:42 +1200

    Halfway through variable types

---
+++ variable_types.py
@@ -0,0 +1,42 @@
+# Variable Types (Data types)
+
+# * ~~ Numeric Types ~~
+
+# int -> Integer (Whole numbers)
+x = 5
+
+# float -> Decimial numbers
+y = 3.14
+
+# complex -> Complex numbers
+z = 2 + 3j
+
+# * ~~ Text Types ~~
+
+# str -> string of text
+greeting = "Hello, world!"
+
+# * ~~ Booleans Types ~~
+
+# bool -> True or false values
+is_on = True
+
+# * ~~ Sequence Types ~~
+
+# list -> Ordered, changeable, allows dupes
+[1,2,3]
+
+# tuple -> Ordered, immutable, allows dupes
+(1,2,3)
+
+# range -> Immutable sequence of numbers 
+range(5) # -> 0,1,2,3,4
+
+# * ~~ Set Types ~~
+
+# set -> Unique, unordered 
+{1,2,3}
+
+# frozenset -> Immutable set
+frozenset([1, 2])
+
