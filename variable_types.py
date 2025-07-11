af7fc047f7d80e17909b36477d31a904a0128d18
brittany-potato <brittanycc@missionreadyhq.com>
2025-07-11 12:05:45 +1200

    Added file on Operators

--- variable_types.py
+++ variable_types.py
@@ -40,3 +40,40 @@ range(5) # -> 0,1,2,3,4
 # frozenset -> Immutable set
 frozenset([1, 2])
 
+# * ~~ Mapping Types ~~
+
+# dict -> Key-value mapping -> {"name": "Brittany", "age": 27}
+
+person = {
+    "name": "Britt",
+    "age": 27
+}
+
+# * ~~ Binary Types ~~
+
+# bytes -> Immutable binary
+b"Hello"
+
+# bytearray -> Mutable binary
+bytearray([65, 66, 67])
+
+# memoryview -> Memory-efficent 
+memoryview(b"abc")
+
+# * ~~ None Type ~~
+
+# NoneType -> No value/Null
+value = None
+
+# * ~~ Type Checking ~~
+
+# Use type() function:
+print(type("Hello")) # <class 'str'>
+
+# * ~~ Type Conversion Examples ~~
+
+int("42") # -> 42
+float("3.14") # -> 3.14
+str(123) # -> '123'
+list('abc') # -> ['a', 'b', 'c']
+dict([("a", 1)]) # -> {'a': 1}
