9a2cc9eda044cc9a25bd0889f7d472b1c79a6634
brittany-potato <brittanycc@missionreadyhq.com>
2025-09-29 14:13:42 +1300

    Completed error handling file

--- error_handling.py
+++ error_handling.py
@@ -30,9 +30,12 @@ except ValueError:
 risky_code = "Stop screaming at me about squiggles"
 
 try:
-    risky_code()
+    # This code will cause a ZeroDevisionError
+    result = 10 / 0
 except Exception as e:
-    print("An error occurred:", e)
+    # The 'e' variable contains the exception object
+    print(f"An error occured: {e}")
+    print(f"The type of error was: {type(e).__name__}")
     
 # * ~~ else Clause ~~
 
@@ -78,6 +81,22 @@ try:
 except TooSmallError as e:
     print(e)
     
+# * ~~ The 'with' Statement (Context managers) ~~
+
+#? The 'with' statement simplifies resource management (like files) by ensuring
+#? that cleanup operations (like f.close()) are always performed,
+#? even if an error occurs inside the block. It is the recommended way to handle files.
+
+# This code is cleaner and safer than the try/finally block for files.
+try:
+    with open("non_existent_file.txt", "r") as f:
+        content = f.read()
+    print(content)
+except FileNotFoundError:
+    print("File not found. The 'with' statement already handled closing.")
+
+
+    
 # * ~~ Common Built-in Exceptions ~~
 
 # ValueError -> Invalid value (e.g., int("abc"))
@@ -95,4 +114,21 @@ except TooSmallError as e:
 # AttributeError -> Object has no such attribute
 
 # ImportError -> Module/Function can't be imported
+
+# NameError -> A variable is not found (e.g. print(my_variable) before it is defined)
+
+# SyntaxError -> The code s not valid python (e.g. missing a colon)
+
+# IndentationError -> Incorrect indentation
+
+# KeyboardInterupt -> The user hits Ctrl + c
+
+#* ~~ Assert - for debugging ~~
+# Raises an AeertationError if a condition is not met.
+# Can be disabled  in production for performance.
+
+user_role = 'guest'
+assert user_role == "admin", "User must be an admin to proceed!"
+# This will raise: AssertionError: User must be an admin to proceed!
+
 
